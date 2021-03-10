42fa149b38eeeac10873c4ac8b01e61d61e9d393
Maxim Safronov <safronov87@gmail.com>
2021-03-10 22:28:17 +0300

    LinuxMain file added

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maxim Safronov on 10.03.2021.
+//
+
+import XCTest
+@testable import MarathonTests
+
+XCTMain([
+     testCase(MarathonTests.allTests)
+])
