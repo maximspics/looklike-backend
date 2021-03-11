1bc8193a072874c7a63426dc2dd187b554c0e9a1
Maxim Safronov <safronov87@gmail.com>
2021-03-11 12:21:29 +0300

    Procfile

---
+++ Sources/App/Migrations/CreateUser.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maxim Safronov on 11.03.2021.
+//
+
+import Foundation
+import Fluent
+import FluentPostgresDriver
+
+struct CreateUser: Migration {
+    
+    func prepare(on database: Database) -> EventLoopFuture<Void> {
+        
+        let result = database.schema("users")
+            .id()
+            .field("user_id", .int, .required)
+            .field("email", .string, .required)
+            .field("password", .string, .required)
+            .field("first_name", .string, .required)
+            .field("last_name", .string)
+            .create()
+        
+        let _ = User(userId: 1, email: "1", password: "1", firstName: "Админ", lastName: "Админович").save(on: database)
+        
+        return result
+    }
+    
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("users").delete()
+    }
+    
+}
