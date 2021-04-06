1f5093c25bda82f3dd29a4a9ecf557379d53a008
Maxim Safronov <safronov87@gmail.com>
2021-04-06 23:01:53 +0300

    Configure file changed

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,9 +5,7 @@ import FluentPostgresDriver
 
 // configures your application
 public func configure(_ app: Application) throws {
-    
-    // Ниже данные БД из Хероку
-   
+    /*
     if let databaseURL = Environment.get("DATABASE_URL") {
         app.databases.use(try .postgres(
             url: databaseURL
@@ -15,22 +13,14 @@ public func configure(_ app: Application) throws {
     } else {
         // ...
     }
-    /*
-     let postgresConfiguration = PostgresConfiguration(hostname: "ec2-54-161-239-198.compute-1.amazonaws.com", port: 5432, username: "uicadcxwlhhbsb", password: "72d6eeb5d966d4e11f9ef1cb45d15f4cb2a80a1813b5ed24718c7134c9df428d", database: "d4n2n1rpodbp31", tlsConfiguration: .clientDefault)
-     
-     app.databases.use(DatabaseConfigurationFactory.postgres(configuration: postgresConfiguration), as: .psql)
-     
-     app.databases.use(.postgres(hostname: "localhost", username: "", password: "", database: "looklike_db"), as: .psql)
-     */
-    
-    
-  //  app.databases.use(.sqlite(.file("looklike_bd_2.sqlite")), as: .sqlite)
- //   app.databases.use(.sqlite(.file("/Users/maximsafronov/Documents/Git/looklike-backend/looklike_db.sqlite")), as: .sqlite)
+    */
+    app.databases.use(.sqlite(.file("looklike_bd_2.sqlite")), as: .sqlite)
+
     app.migrations.add(CreateUser())
     app.migrations.add(CreateProduct())
     app.migrations.add(CreateCategory())
     
-  //  try app.autoMigrate().wait()
+    try app.autoMigrate().wait()
     
- //   try routes(app)
+    try routes(app)
 }
